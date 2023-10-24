721d4c331953619ef1bb118bc871dabc98facabb
Affan Dhia Ardhiva <affandhia@gmail.com>
2023-10-24 16:54:41 +0800

    Remove addons docs

--- packages/kossan-ui/.storybook/main.js
+++ packages/kossan-ui/.storybook/main.js
@@ -16,7 +16,6 @@ const config = {
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-interactions'),
-    // getAbsolutePath('@storybook/addon-docs'),
   ],
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),

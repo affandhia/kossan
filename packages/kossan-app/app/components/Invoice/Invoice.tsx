d4d8eed3b078526549c99ca72ddcd15960468704
Affan Dhia Ardhiva <affandhia@gmail.com>
2023-10-24 08:39:55 +0800

    Remove unnecessary Invoice components

--- packages/kossan-app/app/components/Invoice/Invoice.tsx
+++
@@ -1,7 +0,0 @@
-import { Invoice as InvoiceComponent, InvoiceProps } from '@kossan/design-system';
-
-const Invoice = ({ data }: InvoiceProps) => (
-    <InvoiceComponent data={data} />
-  );
-
-export default Invoice;

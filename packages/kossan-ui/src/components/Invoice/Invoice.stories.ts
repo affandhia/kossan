28eed467a4c2cbe96b7cf1cd783a0917dcab6467
Affan Dhia Ardhiva <affandhia@gmail.com>
2023-10-24 12:16:04 +0800

    Add Invoice storybook

---
+++ packages/kossan-ui/src/components/Invoice/Invoice.stories.ts
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Component from './Invoice';
+import DefaultData from './constants';
+
+const meta: Meta<typeof Component> = {
+  component: Component,
+};
+
+export default meta;
+type Story = StoryObj<typeof Component>;
+
+export const Unchecked: Story = {
+  args: {
+    data: DefaultData,
+  },
+};
